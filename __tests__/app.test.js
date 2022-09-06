76028530df1f86b0c00e34a68e69d6ffa3923f9e
markbolsover <markbolsover@outlook.com>
2022-09-06 12:05:37 +0100

    adds get article by id functionality

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,14 +28,47 @@ describe('/api/topics', () => {
                     });
                 })
         });
-    });
-    describe('GET', () => {
         test('404: client makes request with invalid url', () => {
             return request(app)
-                .get('/api/elephant')
+                .get('/api/topic')
                 .expect(404)
                 .then((res) => {
                     expect(res.body.msg).toBe('route not found');
+                });
+        });
+    });
+});
+
+describe('/api/articles', () => {
+    describe('GET', () => {
+        test('200: responds with an article object with the correct properties', () => {
+            return request(app)
+                .get('/api/articles/5')
+                .expect(200)
+                .then((res) => {
+                    expect(res.body.article).toHaveProperty('article_id', expect.any(Number));
+                    expect(res.body.article).toHaveProperty('title', 'UNCOVERED: catspiracy to bring down democracy');
+                    expect(res.body.article).toHaveProperty('topic', 'cats');
+                    expect(res.body.article).toHaveProperty('author', 'rogersop');
+                    expect(res.body.article).toHaveProperty('body', 'Bastet walks amongst us, and the cats are taking arms!');
+                    expect(res.body.article).toHaveProperty('created_at', '2020-08-03T13:14:00.000Z');
+                    expect(res.body.article).toHaveProperty('votes', 0);
+                });
+        });
+        test('404: client makes request for an id that does not exist', () => {
+            return request(app)
+                .get('/api/articles/15')
+                .expect(404)
+                .then((res) => {
+                    expect(res.body.msg).toBe('article not found');
+                });
+        });
+        test('400: client makes an invalid request, using title for query instead of id', () => {
+            return request(app)
+                .get('/api/articles/Living in the shadow of a great man')
+                .expect(400)
+                .then((res) => {
+                    expect(res.body.msg).toBe('bad request');
                 });
         });
     });
--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const { getTopics } = require('./controllers/topics.controllers.js');
+const { getTopics } = require('./controllers/topics.controller.js');
+const { getArticleByID } = require('./controllers/articles.controller.js');
 const { errorHandler } = require('./error-handling.js');
 
 const app = express();
 
 app.get('/api/topics', getTopics);
+app.get('/api/articles/:article_id', getArticleByID);
 app.all('/*', errorHandler);
 
 app.use((err, req, res, next) => {
-    if (err.status && err.msg) {
+    if (err.code === '22P02') {
+        res.status(400).send({ msg: 'bad request' });
+    } else if (err.status && err.msg) {
         res.status(err.status).send({ msg: err.msg });
-    }
-    else {
+    } else {
         console.log(err, '<<<<<<<<< error');
         res.status(500).send({ msg: 'internal server error' });
-    }
+    };
 });
 
 module.exports = app;
