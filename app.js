c62fb04a40ff1edce8393525c7299ea0579975c0
markbolsover <markbolsover@outlook.com>
2022-09-08 11:40:46 +0100

    add get articles functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const { getTopics } = require('./controllers/topics.controller.js');
 const { getUsers } = require('./controllers/users.controller.js');
-const { getArticleByID, editVotes } = require('./controllers/articles.controller.js');
+const { getArticles, getArticleByID, editVotes } = require('./controllers/articles.controller.js');
 const { invalidRequest } = require('./controllers/errors.controller.js');
 
 const app = express();
@@ -11,6 +11,7 @@ app.use(express.json());
 
 app.get('/api/topics', getTopics);
 app.get('/api/users', getUsers);
+app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id', getArticleByID);
 app.patch('/api/articles/:article_id', editVotes);
 app.all('/*', invalidRequest);
--- models/articles.model.js
+++ models/articles.model.js
@@ -1,6 +1,28 @@
 const db = require('../db/connection.js');
 
-exports.selectArticle = (articleId) => {
+exports.selectArticles = (topic) => {
+    let queryStr = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, 
+                        CAST(COUNT(comments.*) AS int) AS comment_count FROM articles
+                        LEFT JOIN comments ON comments.article_id = articles.article_id`;
+    const queryValues = [];
+    if (topic) {
+        queryStr += ` WHERE topic = $1`;
+        queryValues.push(topic);
+    }
+    queryStr += ` GROUP BY articles.article_id
+                    ORDER BY articles.created_at DESC;`;
+    return db
+        .query(queryStr, queryValues)
+        .then((result) => {
+            if (result.rows.length === 0) {
+                return Promise.reject({ status: 404, msg: 'topic not found' });
+            } else {
+                return result.rows;
+            };
+        });
+};
+
+exports.selectArticleById = (articleId) => {
     return db
         .query(`SELECT articles.*, CAST(COUNT(comments.*) AS int) AS comment_count FROM articles
                 LEFT JOIN comments ON comments.article_id = articles.article_id
